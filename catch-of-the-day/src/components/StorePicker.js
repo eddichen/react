faa061e12a5d3afe8441239aecf94fa3876d8765
eddichen <echen@inviqa.com>
2018-04-26 17:12:56 +0100

    updating new routing

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import { getFunName } from '../helpers';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    goToStore(event) {
-        event.preventDefault();
-        //first grab the test from the box
-        const storeId = this.storeInput.value;
-
-        this.context.router.transitionTo(`store/${storeId}`);
-    }
-
     render() {
         return (
-            <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+            <form className="store-selector">
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit Store</button>
             </form>
         )
     }
-}
-
-StorePicker.contextTypes = {
-    router: React.PropTypes.object
 }
 
 export default StorePicker;
