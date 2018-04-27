54a8e2dc1403d8c2713614ff07435022c53c4f33
eddichen <echen@inviqa.com>
2018-04-27 15:51:41 +0100

    lesson 13

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+    createFish = (event) => {
+        event.preventDefault();
+        const fish = {
+            name: this.nameRef.value,
+            price: parseFloat(this.priceRef.value),
+            status: this.statusRef.value,
+            desc: this.descRef.value,
+            image: this.imageRef.value
+        }
+        this.props.addFish(fish);
+        //refresh the form
+        event.currentTarget.reset();
+    };
+    render() {
+        return (
+            <form className="fish-edit" onSubmit={this.createFish}>
+                <input name="name" ref={(nameRef) => {this.nameRef = nameRef}} type="text" placeholder="name" />
+                <input name="price" ref={(priceRef) => {this.priceRef = priceRef}} type="text" placeholder="price" />
+                <select name="status" ref={(statusRef) => {this.statusRef = statusRef}}>
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea name="desc" ref={(descRef) => {this.descRef = descRef}} placeholder="desc"></textarea>
+                <input name="image" ref={(imageRef) => {this.imageRef = imageRef}} type="text" placeholder="image" />
+                <button type="submit">+ Add Fish</button>
+            </form>
+        )
+    }
+}
+
+export default AddFishForm;
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+
+    goToStore = event => {
+        event.preventDefault();
+        console.log(this.storeInput.value);
+        const storeName = this.storeInput.value.value;
+        this.props.history.push(`/store/${storeName}`);
+    };
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input 
+                    type="text" 
+                    ref={(input) => {this.storeInput = input}} 
+                    required 
+                    placeholder="Store Name" 
+                    defaultValue={getFunName()} 
+                />
                 <button type="submit">Visit Store</button>
             </form>
         )
