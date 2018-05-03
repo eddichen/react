2ff0e4e33b8ce7ab5f72c3dfff45586e06285dae
eddichen <echen@inviqa.com>
2018-05-03 14:17:06 +0100

    building search functionality

--- where-to-lunch/src/components/App.js
+++ where-to-lunch/src/components/App.js
@@ -1,24 +1,50 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { Typeahead } from "react-bootstrap-typeahead";
+import EateryList from "./EateryList";
 import base from "../base";
-import Eatery from "./Eatery";
 
 class App extends Component {
   state = {
-    eateries: {}
+    eateries: {},
+    tags: [],
+    eateriesFiltered: []
   };
 
   componentDidMount() {
-    this.ref = base.syncState(`eateries`, {
+    this.refEateries = base.syncState(`eateries`, {
       context: this,
       state: "eateries"
+    });
+
+    this.refTags = base.syncState(`tags`, {
+      context: this,
+      state: "tags",
+      asArray: true
     });
   }
 
   componentWillUnmount() {
-    base.removeBinding(this.ref);
+    base.removeBinding(this.refEateries);
+    base.removeBinding(this.refTags);
   }
+
+  filterEateries = selected => {
+    let eateriesFiltered = [];
+    const eateries = { ...this.state.eateries };
+    Object.keys(eateries).map(key => {
+      let tags = eateries[key].tags;
+      if (tags) {
+        for (let i = 0; i < tags.length; i++) {
+          if (tags[i] === selected[0]) {
+            eateriesFiltered.push(eateries[key]);
+          }
+        }
+      }
+    });
+    console.log(eateriesFiltered);
+    this.setState({ eateriesFiltered: eateriesFiltered });
+  };
 
   render() {
     return (
@@ -27,11 +53,16 @@ class App extends Component {
         <form>
           <div className="form-group">
             <label htmlFor="search">I'm feeling like...</label>
-            <input type="text" name="search" id="search" ref="search" />
-            {/* <Typeahead onChange={} options={} /> */}
-            <button>Search</button>
+            <Typeahead
+              options={this.state.tags}
+              onChange={selected => {
+                this.filterEateries(selected);
+              }}
+              id="search"
+            />
           </div>
         </form>
+        <EateryList eateries={this.state.eateriesFiltered} />
       </div>
     );
   }
