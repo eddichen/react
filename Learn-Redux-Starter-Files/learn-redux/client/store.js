ac3bc9dc1ba39e3204f7c3e0db944aff8a34a3b8
eddichen <contacteddichen@gmail.com>
2018-12-08 16:03:34 +0000

    connecting with redux dev tool

--- Learn-Redux-Starter-Files/learn-redux/client/store.js
+++ Learn-Redux-Starter-Files/learn-redux/client/store.js
@@ -1,12 +1,12 @@
-import { createStore, compose } from 'redux';
-import { syncHistoryWithStore } from 'react-router-redux';
-import { browserHistory } from 'react-router';
+import { createStore, compose } from "redux";
+import { syncHistoryWithStore } from "react-router-redux";
+import { browserHistory } from "react-router";
 
 //import the root reducer
-import rootReducer from './reducers/index';
+import rootReducer from "./reducers/index";
 
-import comments from './data/comments';
-import posts from './data/posts';
+import comments from "./data/comments";
+import posts from "./data/posts";
 
 //create an object for the default data
 const defaultState = {
@@ -14,7 +14,11 @@ const defaultState = {
   comments
 };
 
-const store = createStore(rootReducer, defaultState);
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
